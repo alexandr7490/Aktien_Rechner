9718cbbfc2395429aae5f5460126a62abd3ff770
Alexander <alexandr7490@ya.ru>
2025-08-01 21:08:48 +0200

    App is created, closes #1.

---
+++ src/main/java/com/example/Aktien_Rechner/AktienRechnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aktien_Rechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AktienRechnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AktienRechnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Aktien_Rechner/AktienRechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aktien_Rechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AktienRechnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
